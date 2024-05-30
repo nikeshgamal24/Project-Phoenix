6f346f928f4167a646aef960e9d9fd3289dcbd2f
Nikesh Gamal <nikesh.gamal24@gmail.com>
2024-05-30 13:43:58 +0545

    testing for netlify

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 //creating whiteList in order to ensure your server is accessed by limited domain
 const allowedOrigins = [
   "https://project-phoenix-omega.vercel.app",
+  "https://ncitprojectphoenix.netlify.app",
 ];
 
 module.exports = allowedOrigins;
