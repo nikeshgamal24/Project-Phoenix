75bb8b84d4fd1d2bbe8cce42ab9c034752dac1a6
Nikesh Gamal <nikesh.gamal24@gmail.com>
2024-05-30 14:15:50 +0545

    updated allowed origins for frontend omega

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3500",
-  "https://ncitprojectphoenix.netlify.app",
+  "https://project-phoenix-omega.vercel.app",
 ];
 
 module.exports = allowedOrigins;
