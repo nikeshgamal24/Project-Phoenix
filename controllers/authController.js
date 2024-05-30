8319a111f98401bae5c1486b086956f24efdb819
Nikesh Gamal <nikesh.gamal24@gmail.com>
2024-05-30 14:32:26 +0545

    updated error handling in handleLogin and refresh token

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,79 +10,85 @@ const { createRefreshToken } = require("./createSetTokens/createRefreshToken");
 const { setCookie } = require("./createSetTokens/setCookie");
 
 const handleLogin = async (req, res) => {
-  const { email, password, role } = req.body;
+  try {
+    const { email, password, role } = req.body;
 
-  if (!email || !password)
-    return res.status(400).json({
-      message: "Username and password are required to login!",
-    });
+    if (!email || !password)
+      return res.status(400).json({
+        message: "Username and password are required to login!",
+      });
 
-  // check for user found or not
-  let foundUser;
-  if (role === roleList.Student) {
-    foundUser = await Student.findOne({
-      email: email,
-      role: { $in: [role] },
-    }).exec();
-  } else if (role === roleList.Supervisor) {
-    foundUser = await Supervisor.findOne({
-      email: email,
-      role: { $in: [role] },
-    }).exec();
-  } else if (role === roleList.Admin) {
-    foundUser = await Admin.findOne({
-      email: email,
-      role: { $in: [role] },
-    }).exec();
-  } else {
+    // check for user found or not
+    let foundUser;
+    if (role === roleList.Student) {
+      foundUser = await Student.findOne({
+        email: email,
+        role: { $in: [role] },
+      }).exec();
+    } else if (role === roleList.Supervisor) {
+      foundUser = await Supervisor.findOne({
+        email: email,
+        role: { $in: [role] },
+      }).exec();
+    } else if (role === roleList.Admin) {
+      foundUser = await Admin.findOne({
+        email: email,
+        role: { $in: [role] },
+      }).exec();
+    } else {
+      return res.sendStatus(400);
+    }
+
+    if (!foundUser)
+      return res.status(401).json({
+        message: "Unauthorized User", //401 ---> Unauthorized user
+      });
+
+    //check for the password match
+    const match = await bcrypt.compare(password, foundUser.password);
+
+    if (match) {
+      const role = Object.values(foundUser.role);
+
+      //create JWTs for authorization
+      //creating access token
+      const accessToken = createAccessToken(
+        foundUser,
+        role,
+        process.env.ACCESS_TOKEN_EXPIRATION_TIME
+      );
+      if (!accessToken)
+        return res.status(400).send("Access Token creation fail");
+
+      //creating refresh token
+      const refreshToken = createRefreshToken(
+        foundUser,
+        process.env.REFRESH_TOKEN_EXPIRATION_TIME
+      );
+      if (!refreshToken)
+        return res.status(400).send("Refresh Token creation fail");
+
+      // sving refreshToken with currrent user
+      foundUser.refreshToken = refreshToken;
+      const result = await foundUser.save();
+      // saving refreshToken to the cookie
+      setCookie(res, refreshToken);
+
+      foundUser.password = undefined;
+      foundUser.refreshToken = undefined;
+      //sending accessToken as an response
+      res.status(200).json({
+        accessToken,
+        user: foundUser,
+      });
+    } else {
+      return res.status(401).json({
+        message: "Unauthorized User", //401 ---> Unauthorized user
+      });
+    }
+  } catch (err) {
+    console.log(`error-message`);
     return res.sendStatus(400);
-  }
-
-  if (!foundUser)
-    return res.status(401).json({
-      message: "Unauthorized User", //401 ---> Unauthorized user
-    });
-
-  //check for the password match
-  const match = await bcrypt.compare(password, foundUser.password);
-
-  if (match) {
-    const role = Object.values(foundUser.role);
-
-    //create JWTs for authorization
-    //creating access token
-    const accessToken = createAccessToken(
-      foundUser,
-      role,
-      process.env.ACCESS_TOKEN_EXPIRATION_TIME
-    );
-    if (!accessToken) return res.status(400).send("Access Token creation fail");
-
-    //creating refresh token
-    const refreshToken = createRefreshToken(
-      foundUser,
-      process.env.REFRESH_TOKEN_EXPIRATION_TIME
-    );
-    if (!refreshToken)
-      return res.status(400).send("Refresh Token creation fail");
-
-    // sving refreshToken with currrent user
-    foundUser.refreshToken = refreshToken;
-    const result = await foundUser.save();
-    // saving refreshToken to the cookie
-    setCookie(res, refreshToken);
-
-    foundUser.password = undefined;
-    foundUser.refreshToken = undefined;
-    //sending accessToken as an response
-    res.status(200).json({
-      accessToken,
-      user: foundUser,
-    });
-  } else {
-    return res.status(401).json({
-      message: "Unauthorized User", //401 ---> Unauthorized user
-    });
   }
 };
 
@@ -99,7 +105,7 @@ const handleEvaluatorLogin = async (req, res) => {
     if (!evaluators) {
       return res.sendStatus(204);
     }
-   
+
     //search for the evaluator with the access code after hashing
     let accessCodeMatched = false;
     let accessToken;
@@ -109,7 +115,10 @@ const handleEvaluatorLogin = async (req, res) => {
       console.log("before accessCodestatus");
       console.log(accessCode, evaluator.accessCode);
       if (evaluator.accessCode) {
-        const accessCodeStatus = await bcrypt.compare(accessCode, evaluator.accessCode);
+        const accessCodeStatus = await bcrypt.compare(
+          accessCode,
+          evaluator.accessCode
+        );
         console.log(accessCodeStatus);
         if (accessCodeStatus) {
           //create JWTs for authorization
@@ -121,18 +130,17 @@ const handleEvaluatorLogin = async (req, res) => {
           );
           foundUser = evaluator;
           accessCodeMatched = true;
-          break;  // Exit the loop as we've found a match
+          break; // Exit the loop as we've found a match
         }
       }
     }
-    
 
     //if accessCode is not matched
     if (!accessCodeMatched) return res.sendStatus(401);
 
     //if something went wrong while creating access token
     if (!accessToken) return res.status(400).send("Access Token creation fail");
-    //removing access code while sending resposne 
+    //removing access code while sending resposne
     foundUser.accessCode = undefined;
     //sending accessToken as an response
     return res.status(200).json({
@@ -144,4 +152,4 @@ const handleEvaluatorLogin = async (req, res) => {
     return res.sendStatus(400);
   }
 };
-module.exports = { handleLogin ,handleEvaluatorLogin};
+module.exports = { handleLogin, handleEvaluatorLogin };
--- controllers/refreshTokenController.js
+++ controllers/refreshTokenController.js
@@ -6,33 +6,46 @@ const jwt = require("jsonwebtoken");
 const { createAccessToken } = require("./createSetTokens/createAccessToken");
 
 const handleRefreshToken = async (req, res) => {
-  const cookies = req.cookies;
+  try {
+    const cookies = req.cookies;
 
-  if (!cookies?.jwt) return res.sendStatus(401); //Unauthorized
+    if (!cookies?.jwt) return res.sendStatus(401); //Unauthorized
 
-  const refreshToken = cookies.jwt;
-  // check for user found or not
-  const foundUser =
-    (await Admin.findOne({ refreshToken }).exec()) ??
-    (await Student.findOne({ refreshToken }).exec()) ??
-    (await Supervisor.findOne({ refreshToken }).exec());
+    const refreshToken = cookies.jwt;
+    // check for user found or not
+    const foundUser =
+      (await Admin.findOne({ refreshToken }).exec()) ??
+      (await Student.findOne({ refreshToken }).exec()) ??
+      (await Supervisor.findOne({ refreshToken }).exec());
 
-  if (!foundUser) return res.sendStatus(403);
-  // console.log(foundUser);
+    if (!foundUser) return res.sendStatus(403);
+    // console.log(foundUser);
 
-  //evaluate jwt for creating access token
-  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
+    //evaluate jwt for creating access token
+    jwt.verify(
+      refreshToken,
+      process.env.REFRESH_TOKEN_SECRET,
+      (err, decoded) => {
+        if (err || foundUser.email !== decoded.email)
+          return res.sendStatus(403);
 
-    if (err || foundUser.email !== decoded.email) return res.sendStatus(403);
+        const role = Object.values(foundUser.role);
+        //create access token from refresh token
+        const accessToken = createAccessToken(
+          foundUser,
+          role,
+          process.env.ACCESS_TOKEN_EXPIRATION_TIME
+        );
 
-    const role = Object.values(foundUser.role);
-    //create access token from refresh token
-    const accessToken =createAccessToken(foundUser,role,process.env.ACCESS_TOKEN_EXPIRATION_TIME);
-  
-    foundUser.password = undefined;
-    foundUser.refreshToken = undefined;
-    res.json({ accessToken, user: foundUser });
-  });
+        foundUser.password = undefined;
+        foundUser.refreshToken = undefined;
+        res.json({ accessToken, user: foundUser });
+      }
+    );
+  } catch (err) {
+    console.log(`error-message:${err.message}`);
+    return res.sendStatus(400);
+  }
 };
 
 module.exports = { handleRefreshToken };
