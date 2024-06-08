aae0c8548e8b9657e587b770c6c3f850a6f75b50
Nikesh Gamal <nikesh.gamal24@gmail.com>
2024-06-08 13:22:33 +0545

    testing for all origin :capacitor

--- controllers/createSetTokens/setCookie.js
+++ controllers/createSetTokens/setCookie.js
@@ -2,7 +2,7 @@ const setCookie = (res, refreshToken) => {
   // saving refreshToken to the cookie
   res.cookie("jwt", refreshToken, {
     httpOnly: true,
-    sameSite: "Strict",
+    sameSite: "None",
     secure: true,
     maxAge: 24 * 60 * 60 * 1000,
     // domain: "project-phoenix-omega.vercel.app",
