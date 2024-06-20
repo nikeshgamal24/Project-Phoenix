20c6c04fba2d8dae03304e4e027d5ff7c943dc42
Nikesh Gamal <nikesh.gamal24@gmail.com>
2024-06-20 15:39:50 +0545

    judgementConfig object for proposal and final defense

---
+++ config/judgementConfig.js
@@ -0,0 +1,9 @@
+const judgementConfig = {
+    0: "Accepted",
+    1: "Accepted Conditionally",
+    2: "Re-Demo",
+    3: "Re-Defend",
+    "-1": "Absent",
+  };
+
+module.exports = judgementConfig;
---
+++ config/proposalJudgementConfig.js
@@ -0,0 +1,9 @@
+const proposalJudgementConfig = {
+  "Accepted": "0",
+  "Accepted Conditionally": "1",
+  "Re-Defense": "2",
+  "Rejected": "3",
+  "Absent": "-1",
+};
+
+module.exports = proposalJudgementConfig;
