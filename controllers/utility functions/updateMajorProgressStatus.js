03c18545579ff66c2e7e779a173c38a14dfb2ba3
Nikesh Gamal <nikesh.gamal24@gmail.com>
2024-06-21 22:13:53 +0545

    fix issue of updateprogressstatus by eligibility by changing the function

--- controllers/utility functions/updateMajorProgressStatus.js
+++ controllers/utility functions/updateMajorProgressStatus.js
@@ -1,27 +1,38 @@
 const eligibilityStatus = require("../../config/progressStatusEligibilityCode");
+
 const updateMajorProgressStatus = (eligibility) => {
+  console.log("*****eligibility**********");
+  console.log(eligibility);
   try {
-    switch (eligibility) {
-      case eligibilityStatus.proposal.eligibeForReportSubmission ||
-        eligibilityStatus.proposal.defenseFail ||
-        eligibilityStatus.proposal.createTeam:
-        return "2001";
-      case eligibilityStatus.proposal.eligibleForDefense:
-        return "2002";
-      case eligibilityStatus.proposal.defensePass ||
-        eligibilityStatus.mid.defenseFail ||eligibilityStatus.mid.eligibeForReportSubmission:
-        return "2010";
-      case eligibilityStatus.mid.eligibleForDefense:
-        return "2011";
-      case eligibilityStatus.mid.defensePass ||eligibilityStatus.final.defenseFail ||eligibilityStatus.final.eligibeForReportSubmission:
-        return "2020";
-      case eligibilityStatus.final.eligibleForDefense:
-        return "2021";
-      case eligibilityStatus.final.defensePass:
-        return "3000";
+    if (
+      eligibility === eligibilityStatus.proposal.eligibeForReportSubmission ||
+      eligibility === eligibilityStatus.proposal.defenseFail ||
+      eligibility === eligibilityStatus.proposal.createTeam
+    ) {
+      return "2001";
+    } else if (eligibility === eligibilityStatus.proposal.eligibleForDefense) {
+      return "2002";
+    } else if (
+      eligibility === eligibilityStatus.proposal.defensePass ||
+      eligibility === eligibilityStatus.mid.defenseFail ||
+      eligibility === eligibilityStatus.mid.eligibeForReportSubmission
+    ) {
+      return "2010";
+    } else if (eligibility === eligibilityStatus.mid.eligibleForDefense) {
+      return "2011";
+    } else if (
+      eligibility === eligibilityStatus.mid.defensePass ||
+      eligibility === eligibilityStatus.final.defenseFail ||
+      eligibility === eligibilityStatus.final.eligibeForReportSubmission
+    ) {
+      return "2020";
+    } else if (eligibility === eligibilityStatus.final.eligibleForDefense) {
+      return "2021";
+    } else if (eligibility === eligibilityStatus.final.defensePass) {
+      return "3000";
     }
   } catch (err) {
-    console.error(`error-message:${err.message}`);
+    console.error(`error-message-updating-progressStatus:${err.message}`);
     return null;
   }
 };
