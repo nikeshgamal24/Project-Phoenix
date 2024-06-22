58da0a8eb76be8edde48e77ed0733a5bea7bfb36
Nikesh Gamal <nikesh.gamal24@gmail.com>
2024-06-22 10:56:23 +0545

    changed updateProjectFirstProgressStatus

--- controllers/utility functions/updateProjectFirstProgressStatus.js
+++ controllers/utility functions/updateProjectFirstProgressStatus.js
@@ -1,22 +1,29 @@
 const eligibilityStatus = require("../../config/progressStatusEligibilityCode");
+
 const updateProjectFirstProgressStatus = (eligibility) => {
   try {
     switch (eligibility) {
-      case eligibilityStatus.proposal.eligibeForReportSubmission ||
-        eligibilityStatus.proposal.defenseFail||eligibilityStatus.proposal.createTeam:
+      case eligibilityStatus.proposal.eligibeForReportSubmission:
+      case eligibilityStatus.proposal.defenseFail:
+      case eligibilityStatus.proposal.createTeam:
         return "0001";
       case eligibilityStatus.proposal.eligibleForDefense:
         return "0002";
-      case eligibilityStatus.proposal.defensePass ||
-        eligibilityStatus.final.defenseFail||eligibilityStatus.final.eligibeForReportSubmission:
+      case eligibilityStatus.proposal.defensePass:
+      case eligibilityStatus.final.defenseFail:
+      case eligibilityStatus.final.eligibeForReportSubmission:
         return "0010";
       case eligibilityStatus.final.eligibleForDefense:
         return "0011";
       case eligibilityStatus.final.defensePass:
         return "1000";
+      default:
+        // Handle any unexpected values
+        console.error(`Unexpected eligibility value: ${eligibility}`);
+        return null;
     }
   } catch (err) {
-    console.error(`error-message:${err.message}`);
+    console.error(`error-message: ${err.message}`);
     return null;
   }
 };
