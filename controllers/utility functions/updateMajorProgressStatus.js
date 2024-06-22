718bcc8fab01f690192f6c5be70babac598da973
Nikesh Gamal <nikesh.gamal24@gmail.com>
2024-06-22 11:00:40 +0545

    updateMajorPogressStatus function

--- controllers/utility functions/updateMajorProgressStatus.js
+++ controllers/utility functions/updateMajorProgressStatus.js
@@ -4,35 +4,34 @@ const updateMajorProgressStatus = (eligibility) => {
   console.log("*****eligibility**********");
   console.log(eligibility);
   try {
-    if (
-      eligibility === eligibilityStatus.proposal.eligibeForReportSubmission ||
-      eligibility === eligibilityStatus.proposal.defenseFail ||
-      eligibility === eligibilityStatus.proposal.createTeam
-    ) {
-      return "2001";
-    } else if (eligibility === eligibilityStatus.proposal.eligibleForDefense) {
-      return "2002";
-    } else if (
-      eligibility === eligibilityStatus.proposal.defensePass ||
-      eligibility === eligibilityStatus.mid.defenseFail ||
-      eligibility === eligibilityStatus.mid.eligibeForReportSubmission
-    ) {
-      return "2010";
-    } else if (eligibility === eligibilityStatus.mid.eligibleForDefense) {
-      return "2011";
-    } else if (
-      eligibility === eligibilityStatus.mid.defensePass ||
-      eligibility === eligibilityStatus.final.defenseFail ||
-      eligibility === eligibilityStatus.final.eligibeForReportSubmission
-    ) {
-      return "2020";
-    } else if (eligibility === eligibilityStatus.final.eligibleForDefense) {
-      return "2021";
-    } else if (eligibility === eligibilityStatus.final.defensePass) {
-      return "3000";
+    switch (eligibility) {
+      case eligibilityStatus.proposal.eligibeForReportSubmission:
+      case eligibilityStatus.proposal.defenseFail:
+      case eligibilityStatus.proposal.createTeam:
+        return "2001";
+      case eligibilityStatus.proposal.eligibleForDefense:
+        return "2002";
+      case eligibilityStatus.proposal.defensePass:
+      case eligibilityStatus.mid.defenseFail:
+      case eligibilityStatus.mid.eligibeForReportSubmission:
+        return "2010";
+      case eligibilityStatus.mid.eligibleForDefense:
+        return "2011";
+      case eligibilityStatus.mid.defensePass:
+      case eligibilityStatus.final.defenseFail:
+      case eligibilityStatus.final.eligibeForReportSubmission:
+        return "2020";
+      case eligibilityStatus.final.eligibleForDefense:
+        return "2021";
+      case eligibilityStatus.final.defensePass:
+        return "3000";
+      default:
+        // Handle any unexpected values
+        console.error(`Unexpected eligibility value: ${eligibility}`);
+        return null;
     }
   } catch (err) {
-    console.error(`error-message-updating-progressStatus:${err.message}`);
+    console.error(`error-message-updating-progressStatus: ${err.message}`);
     return null;
   }
 };
