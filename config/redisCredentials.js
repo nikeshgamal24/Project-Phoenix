a3c50cfd76dd0eac15dd085215f2171a1a795dad
Nikesh Gamal <nikesh.gamal24@gmail.com>
2024-06-29 21:56:12 +0545

    complete set up for the upstash-redis credentials

---
+++ config/redisCredentials.js
@@ -0,0 +1,5 @@
+const REDIS_URI = process.env.UPSTASH_REDIS_URI;
+const REDIS_PORT = process.env.UPSTASH_REDIS_PORT;
+const REDIS_TOKEN = process.env.UPSTASH_REDIS_TOKEN;
+
+module.exports = { REDIS_URI, REDIS_PORT, REDIS_TOKEN };
